a27ec415f49f411a390948498e6fd4a33834b278
Vladimir Fishchenko <hws47a@gmail.com>
2015-04-01 22:17:42 +0300

    fixed paths

--- magetest.sh
+++ magetest.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 echo "Checking $1"
-phpcs --standard=./vendor/magento-ecg/coding-standard/Ecg $1
-phpmd $1 text ./phpmd/ruleset.xml
+phpcs --standard=$DIR/vendor/magento-ecg/coding-standard/Ecg $1
+phpmd $1 text $DIR/phpmd/ruleset.xml
 phpcpd $1
